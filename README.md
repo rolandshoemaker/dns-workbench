034083ed9d2ddd1ea67115cc430e725c29adb993
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-08-22 15:10:52 -0700

    Various more framework work

--- README.md
+++ README.md
@@ -22,20 +22,3 @@ zones:
     mx:
       - 10 mail.other.com
 ```
-*-- This idea probably won't entire work --*
-
-These zones are parsed by using `miekg/dns` to convert RR name -> record type ->
-record struct. `reflect` is then used to inspect each of the fields on the respective
-struct and properly convert `string` -> the right type. If there aren't enough
-strings in the slice to fill all the fields (or and empty string is provided)
-the field will be left empty. If a string cannot be marshaled to the proper type
-the workbench will fail to load the entire zone file. The zone file will throw
-warnings if too many fields were passed
-
-*-- This on the other handle will but will be more annoying to write --*
-
-...or, It could just convert from name -> record type -> converter func. This would
-allow much finer control over records which would allow much better spec compliance
-(e.g. with `TXT` records and such). The converter functions would know how many
-fields are required, the type specific parsing needed, etcetcetc which would make
-life a whole bunch easier in terms of weird edge cases.
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
 	"strings"
 	"time"
 
 	"gopkg.in/yaml.v2"
 
+	"github.com/codegangsta/cli"
 	"github.com/miekg/dns"
 )
 
@@ -25,6 +29,7 @@ func constrcutZones(rz rawZones) (*zones, error) {
 	for host, records := range rz.Zones {
 		host = dns.Fqdn(host)
 		z.Zones[host] = make(map[uint16][]dns.RR)
+		// Always add generated SOA rr?
 		for typeStr, v := range records {
 			rType, present := dns.StringToType[strings.ToUpper(typeStr)]
 			if !present {
@@ -43,7 +48,15 @@ func constrcutZones(rz rawZones) (*zones, error) {
 }
 
 type workbench struct {
-	z *zones
+	z           *zones
+	name        string
+	bind        string
+	port        string
+	net         string
+	rTimeout    time.Duration
+	wTimeout    time.Duration
+	iTimeout    time.Duration
+	compression bool
 }
 
 func (wb *workbench) updateZones(nz zones) error {
@@ -54,15 +67,21 @@ func (wb *workbench) dnsHandler(w dns.ResponseWriter, r *dns.Msg) {
 	defer w.Close()
 	m := new(dns.Msg)
 	m.SetReply(r)
-	m.Compress = false
-	// Always add generated authority section?
+	m.Compress = wb.compression
 	for _, q := range r.Question {
-		fmt.Printf("dns-srv: Query -- [%s] %s\n", q.Name, dns.TypeToString[q.Qtype])
+		fmt.Printf("[dns-wb] Recieved query for %s [%s]\n", q.Name, dns.TypeToString[q.Qtype])
 		allRecords, present := wb.z.Zones[q.Name]
 		if !present {
 			// NXDOMAIN
 			continue
 		}
+		m.Authoritative = true
+		authRR, err := dns.NewRR(fmt.Sprintf("%s NS %s", q.Name, wb.name))
+		if err != nil {
+			fmt.Printf("Failed to create authority NS record: %v\n", err)
+			continue
+		}
+		m.Ns = append(m.Ns, authRR)
 		qRecords, present := allRecords[q.Qtype]
 		if !present {
 			continue
@@ -76,10 +95,14 @@ func (wb *workbench) dnsHandler(w dns.ResponseWriter, r *dns.Msg) {
 func (wb *workbench) serveWorkbench() {
 	dns.HandleFunc(".", wb.dnsHandler)
 	server := &dns.Server{
-		Addr:         "127.0.0.1:8053",
-		Net:          "udp",
-		ReadTimeout:  time.Millisecond,
-		WriteTimeout: time.Millisecond,
+		Addr:         net.JoinHostPort(wb.bind, wb.port),
+		Net:          wb.net,
+		ReadTimeout:  wb.rTimeout,
+		WriteTimeout: wb.wTimeout,
+		IdleTimeout:  func() time.Duration { return wb.iTimeout },
+		NotifyStartedFunc: func() {
+			fmt.Printf("[dns-wb] Started listening on %s:%s, serving %d zones\n", wb.bind, wb.port, len(wb.z.Zones))
+		},
 	}
 	err := server.ListenAndServe()
 	if err != nil {
@@ -88,36 +111,124 @@ func (wb *workbench) serveWorkbench() {
 	}
 }
 
-var t = `zones:
-  bracewel.net:
-    a:
-      - 1.1.1.1
-      - 2.2.2.2
-    aaaa:
-      - FF01:0:0:0:0:0:0:FB
-    caa:
-      - 0 issue "letsencrypt.org"
-
-  other.com:
-    mx:
-      - 10 mail.other.com`
+func loadYAML(filename string) (rawZones, error) {
+	rz := rawZones{}
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return rz, nil
+	}
+	err = yaml.Unmarshal(content, &rz)
+	return rz, err
+}
 
 func main() {
-	var rz rawZones
-	err := yaml.Unmarshal([]byte(t), &rz)
-	fmt.Printf("%v\n %+v\n", err, rz)
+	app := cli.NewApp()
+	app.Name = "dns-workbench"
+	app.Usage = "a simple authoritative DNS workbench server"
+	app.Authors = []cli.Author{cli.Author{Name: "Roland Shoemaker", Email: "rolandshoemaker@gmail.com"}}
+	app.Version = "0.0.1"
 
-	z, err := constrcutZones(rz)
-	fmt.Printf("%v\n %+v\n", err, z)
+	app.Flags = []cli.Flag{}
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "Starts the DNS server",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "dns-name",
+					Value: "localhost",
+					Usage: "Hostname of the DNS server",
+				},
+				cli.StringFlag{
+					Name:  "dns-address",
+					Value: "127.0.0.1",
+					Usage: "Address for the DNS server to listen on",
+				},
+				cli.StringFlag{
+					Name:  "dns-port",
+					Value: "8053",
+					Usage: "Port for the DNS server to listen on",
+				},
+				cli.StringFlag{
+					Name:  "dns-network",
+					Value: "udp",
+					Usage: "Network for the DNS server to listen on",
+				},
+				cli.BoolFlag{
+					Name:  "dns-compression",
+					Usage: "Use DNS message compression",
+				},
+				cli.StringFlag{
+					Name:  "zone-file",
+					Usage: "Path to workbench zones file",
+				},
+				cli.StringFlag{
+					Name:  "api-uri",
+					Value: "127.0.0.1:5353",
+					Usage: "Address for the HTTP API to listen on",
+				},
+				cli.BoolTFlag{
+					Name:  "disable-api",
+					Usage: "Don't start the HTTP API",
+				},
+			},
+			Action: func(c *cli.Context) {
+				var rz rawZones
+				var z *zones
+				var err error
+				rzFile := c.String("zone-file")
+				if rzFile != "" {
+					rz, err = loadYAML("example.yml")
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					z, err = constrcutZones(rz)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+				} else {
+					z = &zones{Zones: make(map[string]map[uint16][]dns.RR)}
+				}
 
-	wb := workbench{z: z}
-	wb.serveWorkbench()
+				wb := workbench{
+					z:           z,
+					name:        dns.Fqdn(c.String("dns-name")),
+					bind:        c.String("dns-address"),
+					port:        c.String("dns-port"),
+					net:         c.String("dns-network"),
+					rTimeout:    time.Second * 2,
+					wTimeout:    time.Second * 2,
+					iTimeout:    time.Second * 8,
+					compression: c.Bool("dns-compression"),
+				}
+				wb.serveWorkbench()
+			},
+		},
+		{
+			Name:  "reload",
+			Usage: "Loads a new zone file into a running workbench",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "zone-file",
+					Usage: "Path to workbench zones file",
+				},
+				cli.StringFlag{
+					Name:  "api-uri",
+					Value: "127.0.0.1:5353",
+					Usage: "Address for the HTTP API",
+				},
+			},
+			Action: func(c *cli.Context) {
+				// do something
+				fmt.Println(c.String("zone-file"))
+			},
+		},
+	}
 
-	// a := `bracewel.net. CAA 0 issue "letsencrypt.org"`
-	// rr, err := dns.NewRR(a)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// fmt.Printf("%+v\n", rr)
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
